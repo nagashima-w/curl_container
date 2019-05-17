aa4f877bf78c35abcfd626d4103f86f90d266c4d
nagashi_ma_w <nagashima5.wataru@gmail.com>
2019-05-17 14:48:01 +0900

    fix

--- tools/curl.sh
+++ tools/curl.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # テストしたいURLをここに書く
-URL="https://setsuzokusitai.url";
+URL="https://www.google.com/";
 
-for i in {0..100}
+i=1;
+while [ $i -le 100 ];
 # 回数を変えたい場合はここの数字を変える
 do
     SETDT=`date "+%Y%m%d%H%M%S"`
-    curl -o /dev/null $URL
-    sleep 1;
+    curl -o /dev/null $URL;
+    sleep 1s;
 # 間隔を変更したいときはここの数字を変える
 done
+
